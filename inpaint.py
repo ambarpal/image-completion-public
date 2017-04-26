a3c5204040aabd4d4a20b8974be5bec041671c29
ambar <ambar14012@iiitd.ac.in>
2017-04-26 01:38:12 +0000

    Fixed Corrupted Batch Loading

--- inpaint.py
+++ inpaint.py
@@ -18,10 +18,10 @@ def write_Image(img, path, batch_size, sess):
     return out
 
 if __name__ == '__main__':    
-    lbd = 0.5
     
     DATASET = 'MNIST'
-    RANDOM_BLACKOUT = 0.3
+    lbd = 1 # Final loss is loss_cont + lbd * loss_perc
+    RANDOM_BLACKOUT = 0.8
     RUN_NAME = 'random_pixels_%0.1f' % RANDOM_BLACKOUT
     flip_alpha = 0.3
     MODEL_TAG = 'plotting-graphs_alpha_' + str(flip_alpha) + '_' + DATASET
@@ -32,63 +32,64 @@ if __name__ == '__main__':
     Z_feed = np.asarray(loader.load_batch_Z(batch_size), dtype=np.float32)
 
     # Blacking out a fixed percentage of pixels randomly
-    Mask = np.random.choice([0, 1], size=(batch_size,64,64,3), p=[RANDOM_BLACKOUT, 1-RANDOM_BLACKOUT])
-    Mask[:, :, :, 1] = Mask[:, :, :, 2] = Mask[:, :, :, 0]
-
-    corruptedImage = loader.load_batch_X(batch_size) * Mask
+    mask_2D = np.random.choice([0, 1], size=(batch_size,64,64), p=[RANDOM_BLACKOUT, 1-RANDOM_BLACKOUT])
+    corrupted_batch_X = loader.load_corrupted_batch_X(batch_size, mask_2D)
     
     if not os.path.isdir('analysis/inpainting/' + TAG):
         os.makedirs('analysis/inpainting/' + TAG)
-          
-    Z = tf.Variable(Z_feed, name="Z_variable")
-    M = tf.placeholder(tf.float32, shape = [None, 64, 64, 3])	# M is the uncorruption mask for y
-    y = tf.placeholder(tf.float32, shape = [None, 64, 64, 3])	# y is the corrupted image
+    
+    # M = tf.placeholder(tf.float32, shape = [None, 64, 64, 3])	# M is the uncorruption mask for y
+    # y = tf.placeholder(tf.float32, shape = [None, 64, 64, 3])	# y is the corrupted image
+    
+    # with tf.variable_scope("input"):      
+    #     Z = tf.Variable(Z_feed, name="Z_variable")
 
-    with tf.variable_scope("gen"):
-        GZ = generator(Z)
+    # with tf.variable_scope("gen"):
+    #     GZ = generator(Z)
 
-    with tf.variable_scope("disc") as scope:
-        DGZ_raw, DGZ = discriminator(GZ)
-        scope.reuse_variables()
+    # with tf.variable_scope("disc") as scope:
+    #     DGZ_raw, DGZ = discriminator(GZ)
+    #     scope.reuse_variables()
 
-    with tf.variable_scope("loss_calculation"):
-        loss_g = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(DGZ_raw), logits=DGZ_raw)
-        loss_perceptual = tf.reduce_mean(loss_g)
-        uncorr_gz = tf.multiply(M, GZ)
-        uncorr_y = tf.multiply(M, y)
-        loss_contextual = tf.norm(uncorr_gz - uncorr_y, ord = 1)
-        total_loss = loss_contextual + lbd * loss_perceptual
+    # with tf.variable_scope("loss_calculation"):
+    #     loss_g = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(DGZ_raw), logits=DGZ_raw)
+    #     loss_perceptual = tf.reduce_mean(loss_g)
+    #     uncorr_gz = tf.multiply(M, GZ)
+    #     uncorr_y = tf.multiply(M, y)
+    #     loss_contextual = tf.reduce_mean(tf.norm(uncorr_gz - uncorr_y, ord = 1, axis=0))
+    #     total_loss = loss_contextual + lbd * loss_perceptual
 
-    optimizer_z = tf.train.AdamOptimizer(learning_rate=2e-4, beta1 = 0.5)
-    train_op_z = optimizer_z.minimize(total_loss, var_list=[Z])
+    # optimizer_z = tf.train.AdamOptimizer(learning_rate=2e-4, beta1 = 0.5)
+    # train_op_z = optimizer_z.minimize(total_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "input"))
 
-    recon_img = tf.multiply(M, y) + tf.multiply((tf.ones_like(M) - M), GZ)
-    GZ_summary = tf.summary.image('GZ', GZ, max_outputs = 10)
-    recon_img_summary = tf.summary.image('recon_img', recon_img, max_outputs = 10)
-    DGZ_summary = tf.summary.scalar('DGZ', tf.reduce_mean(DGZ))
-    loss_per_summary = tf.summary.scalar('loss_perceptual', loss_perceptual)
-    loss_cont_summary = tf.summary.scalar('loss_contextual', loss_contextual)
-    gen_merged = tf.summary.merge_all()
+    # recon_img = tf.multiply(M, y) + tf.multiply((tf.ones_like(M) - M), GZ)
+    # GZ_summary = tf.summary.image('GZ', GZ, max_outputs = 10)
+    # recon_img_summary = tf.summary.image('recon_img', recon_img, max_outputs = 10)
+    # DGZ_summary = tf.summary.scalar('DGZ', tf.reduce_mean(DGZ))
+    # loss_per_summary = tf.summary.scalar('loss_perceptual', loss_perceptual)
+    # loss_cont_summary = tf.summary.scalar('loss_contextual', loss_contextual)
+    # total_loss_summary = tf.summary.scalar('total_loss', total_loss)
+    # gen_merged = tf.summary.merge_all()
 
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
     
-    write_Image(corruptedImage, 'analysis/inpainting/'+TAG+'/corrupted.png', batch_size, sess)
+    write_Image(corrupted_batch_X, 'analysis/inpainting/'+TAG+'/corrupted.png', batch_size, sess)
 
-    train_writer = tf.summary.FileWriter('logs/inpainting/' + TAG, sess.graph)
-    model_name = tf.train.latest_checkpoint('logs/' + MODEL_TAG + '/')
-    variables_to_restore = [x for x in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if "gen" in x.name.lower() or "disc" in x.name.lower()]
-    print len(variables_to_restore)
-    saver = tf.train.Saver(var_list=variables_to_restore)
-    saver.restore(sess, model_name)
+    # train_writer = tf.summary.FileWriter('logs/inpainting/' + TAG, sess.graph)
+    # model_name = tf.train.latest_checkpoint('logs/' + MODEL_TAG + '/')
+    # variables_to_restore = [x for x in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if "gen" in x.name.lower() or "disc" in x.name.lower()]
+    # saver = tf.train.Saver(var_list=variables_to_restore)
+    # saver.restore(sess, model_name)
 
-    num_train_iterations = 10000
-    for iteration in range(num_train_iterations):
-        GZ_ret, recon_img_ret, total_loss_ret, gen_merged_ret = sess.run(fetches=[GZ, recon_img, total_loss, gen_merged], feed_dict={M:Mask, y: corruptedImage})
-        train_writer.add_summary(gen_merged_ret, iteration)
-        print "#%d: Loss: %d" % (iteration, total_loss_ret)
+    # num_train_iterations = 10000
+    # for iteration in range(num_train_iterations):
+    #     GZ_ret, recon_img_ret, total_loss_ret, gen_merged_ret = sess.run(fetches=[GZ, recon_img, total_loss, gen_merged], feed_dict={M:Mask, y: corrupted_batch_X})
+    #     train_writer.add_summary(gen_merged_ret, iteration)
+    #     print "#%d: Loss: %d" % (iteration, total_loss_ret)
         
-        out = write_Image(recon_img_ret, 'analysis/inpainting/'+TAG+'/reconstructed_%d.png'%iteration, batch_size, sess)
-        cv2.imwrite('analysis/inpainting/'+TAG+'/reconstructed_latest.png', out)
+    #     if iteration % 100 == 0:
+    #         out = write_Image(recon_img_ret, 'analysis/inpainting/'+TAG+'/reconstructed_%d.png'%iteration, batch_size, sess)
+    #         cv2.imwrite('analysis/inpainting/'+TAG+'/reconstructed_latest.png', out)
         
-    train_writer.close()
+    # train_writer.close()
--- load_Data_new.py
+++ load_Data_new.py
@@ -4,59 +4,71 @@ from helper import normalize_batch
 from tensorflow.examples.tutorials.mnist import input_data
 import pdb
 import scipy.io as sio
+import os
 
 class DataLoader:
     def __init__(self, dataset_name='MNIST'):
         print "Loading Data..."
-        if(dataset_name == 'MNIST'):
-            self.dataset = input_data.read_data_sets("data/MNIST/")
-            self.data = self.dataset.train.images
-            self.labels = np.array(self.dataset.train.labels)
-            self.data = self.data.reshape(-1,28,28)
-            temp = np.zeros((self.data.shape[0],3,28,28))
-            for i in range(3):
-                temp[:,i,:,:] = self.data.copy()
-            self.data = temp 
-            
-        elif (dataset_name == 'CIFAR10'):
-            self.data = np.empty((0,3072))
-            self.labels = np.empty((0))
-            for i in range(1,6):
-                unpickled = self.unpickle('data/CIFAR/cifar-10-batches-py/data_batch_%d'% i)
-                self.data = np.append(self.data, unpickled['data'],axis=0)
-                self.labels = np.append(self.labels, np.asarray(unpickled['labels']))
-            self.data = self.data.reshape(-1,3,32,32)
+        data_processed_path = 'data/%s/processed' % dataset_name 
+        if not os.path.isdir(data_processed_path):
+            os.makedirs(data_processed_path)
 
-        elif (dataset_name == 'CELEBA'):
-            num_images = 202599
-            self.data = np.zeros((num_images,64,64,3))
-            for i in range(1,num_images+1):
-                img_ = cv2.imread('./data/celebA/%06d.jpg'%i)
-                self.data[i-1] = cv2.resize(img_, None, fx=64.0/img_.shape[1], fy=64.0/img_.shape[0])
-            np.random.shuffle(self.data)
+            if(dataset_name == 'MNIST'):
+                self.dataset = input_data.read_data_sets("data/MNIST/")
+                self.data = self.dataset.train.images
+                self.labels = np.array(self.dataset.train.labels)
+                self.data = self.data.reshape(-1,28,28)
+                temp = np.zeros((self.data.shape[0],3,28,28))
+                for i in range(3):
+                    temp[:,i,:,:] = self.data.copy()
+                self.data = temp 
+                
+            elif (dataset_name == 'CIFAR10'):
+                self.data = np.empty((0,3072))
+                self.labels = np.empty((0))
+                for i in range(1,6):
+                    unpickled = self.unpickle('data/CIFAR/cifar-10-batches-py/data_batch_%d'% i)
+                    self.data = np.append(self.data, unpickled['data'],axis=0)
+                    self.labels = np.append(self.labels, np.asarray(unpickled['labels']))
+                self.data = self.data.reshape(-1,3,32,32)
 
-        elif (dataset_name == 'SVHN'):
-            dataset = sio.loadmat('./data/SVHN/train_32x32.mat')
-            self.data = dataset['X']
-            self.data = np.swapaxes(self.data, 0, 3)
-            self.data = np.swapaxes(self.data, 1, 2)
-            self.data = np.swapaxes(self.data, 2, 3)
-            
-            self.labels = np.reshape(dataset['y'], (dataset['y'].shape[0],))
-            
-        print "pre processing data..."
-        print "loaded data:", self.data.shape
-        if(dataset_name != 'CELEBA'):
-            img_swapped = np.swapaxes(self.data[:], 1, 3).swapaxes(1, 2)
-            print  "swapped axes:", img_swapped.shape
-            temp = np.zeros((self.data.shape[0], 64, 64, 3))
-            for index in range(self.data.shape[0]):
-                temp[index] = cv2.resize(img_swapped[index], None, fx = 64.0/img_swapped.shape[1], fy = 64.0/img_swapped.shape[2])
-            self.data = temp.copy()
-            print "final shape:" ,self.data.shape
-            p = np.random.permutation(range(self.data.shape[0]))
-            self.data = self.data[p]
-            self.labels = self.labels[p]
+            elif (dataset_name == 'CELEBA'):
+                num_images = 202599
+                self.data = np.zeros((num_images,64,64,3))
+                for i in range(1,num_images+1):
+                    img_ = cv2.imread('./data/celebA/%06d.jpg'%i)
+                    self.data[i-1] = cv2.resize(img_, None, fx=64.0/img_.shape[1], fy=64.0/img_.shape[0])
+                np.random.shuffle(self.data)
+                np.save(open(data_processed_path + '/data.npz', 'w'), self.data)
+
+            elif (dataset_name == 'SVHN'):
+                dataset = sio.loadmat('./data/SVHN/train_32x32.mat')
+                self.data = dataset['X']
+                self.data = np.swapaxes(self.data, 0, 3)
+                self.data = np.swapaxes(self.data, 1, 2)
+                self.data = np.swapaxes(self.data, 2, 3)
+                self.labels = np.reshape(dataset['y'], (dataset['y'].shape[0],))
+                
+            print "pre processing data..."
+            print "loaded data:", self.data.shape
+            if(dataset_name != 'CELEBA'):
+                img_swapped = np.swapaxes(self.data[:], 1, 3).swapaxes(1, 2)
+                print  "swapped axes:", img_swapped.shape
+                temp = np.zeros((self.data.shape[0], 64, 64, 3))
+                for index in range(self.data.shape[0]):
+                    temp[index] = cv2.resize(img_swapped[index], None, fx = 64.0/img_swapped.shape[1], fy = 64.0/img_swapped.shape[2])
+                self.data = temp.copy()
+                print "final shape:" ,self.data.shape
+                p = np.random.permutation(range(self.data.shape[0]))
+                self.data = self.data[p]
+                self.labels = self.labels[p]
+                np.save(open(data_processed_path + '/data.npz', 'w'), self.data)
+                np.save(open(data_processed_path + '/labels.npz', 'w'), self.labels)
+        else:
+            self.data = np.load(data_processed_path + '/data.npz')
+            if dataset_name != 'CELEBA':
+                self.labels = np.load(data_processed_path + '/labels.npz')
+
         self.curIdx = 0
         print "Data Loaded and Pre Processed Successfully!"
     
@@ -97,6 +109,23 @@ class DataLoader:
         # batch_z = batch_z / np.linalg.norm(batch_z)
         return batch_z
     
-    # def getCorruptedImage(self, M):
-    #     return M*self.data[np.random.randint(self.data.shape[0])]
+    def load_corrupted_batch_X(self, batch_size, mask_2D, update_iterator = True):
+        batch_x = np.zeros((batch_size, 64, 64,3))
+        length = self.data.shape[0]
+        if self.curIdx + batch_size < length:
+            batch_x = self.data[self.curIdx:self.curIdx + batch_size]
+        else:
+            print "Unhandled reach to end of data"
+            self.curIdx = 0
+            return self.load_batch_X(batch_size)
+        
+        # TODO: Wrap around on reaching end of data     
+        
+        if update_iterator: self.curIdx += batch_size
+
+        mask = np.zeros((batch_size, 64, 64, 3))
+        mask[:, :, :, 1] = mask[:, :, :, 2] = mask[:, :, :, 0] = mask_2D
+        batch_x = np.multiply(batch_x, mask)
+        
+        return normalize_batch(batch_x)
     
